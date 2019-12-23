dba82d5e6aeb536eab063f2f5e9256124afc2ec2
Aboubacar Sogoba <sogoba3@gmail.com>
2019-12-23 01:26:48 -0500

    second commit: Implementing Update Todo features

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -15,11 +15,12 @@ export class TodoComponent implements OnInit {
 
   constructor(
     private todoService: TodoDataService,
-    private router: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
   ) { }
 
   ngOnInit() {
-    this.id = this.router.snapshot.params['id'];
+    this.id = this.route.snapshot.params['id'];
     this.todo = new Todo(1, '', false, new Date());
     this.todoService.retrieveTodo('Aboubacar', this.id).subscribe(
       data => this.todo = data 
@@ -28,6 +29,12 @@ export class TodoComponent implements OnInit {
 
   saveTodo(){
     console.log('save todo', )
+    this.todoService.updateTodo('Aboubacar', this.id, this.todo).subscribe(
+      data => {
+        console.log(data)
+        this.router.navigate(['todos'])
+      }
+    )
   }
 
 }
